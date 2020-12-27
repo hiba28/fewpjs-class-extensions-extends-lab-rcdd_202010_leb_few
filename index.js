fb0a105643779c61d70a85363d57dd9b619113cb
Hiba Abdel karim <hibaabdelkarim2000@gmail.com>
2020-12-27 02:26:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,12 +17,12 @@ class Triangle extends Polygon{
 }
 class Square extends Polygon{
   get isValid(){
-    if(this.countSides != 4) return 
+    if(this.countSides != 4) return
     return
     this.sides[0] === this.sides[1] &&
     this.sides[0] === this.sides[2] &&
     this.sides[0] === this.sides[3] &&
-    
+
   }
   get area(){
     return this.sides[0] ** 2
