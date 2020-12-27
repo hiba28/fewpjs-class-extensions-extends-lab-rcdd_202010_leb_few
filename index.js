f9dcb718d9d49014a8a7aef998ee73abb5292e79
Hiba Abdel karim <hibaabdelkarim2000@gmail.com>
2020-12-27 02:29:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,12 @@ class Polygon {
 }
 class Triangle extends Polygon{
   get isValid(){
-    return this.countSides === 3
+    if ( !Array.isArray( this.sides ) ) return;
+    if ( this.count !== 3 ) return;
+    let side1 = this.sides[ 0 ]
+    let side2 = this.sides[ 1 ]
+    let side3 = this.sides[ 2 ]
+    return ( ( side1 === side2 ) && ( side1 === side3 ) && ( side2 === side3 ) )
   }
 }
 class Square extends Polygon{
