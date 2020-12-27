cb2cd2cd10367c44ae0efe9503a971239fdd1a7c
Hiba Abdel karim <hibaabdelkarim2000@gmail.com>
2020-12-27 02:28:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,13 @@ class Square extends Polygon{
     return
       this.sides[0] === this.sides[1] &&
       this.sides[0] === this.sides[2] &&
-      this.sides[0] === this.sides[3] &&
+      this.sides[0] === this.sides[3] &&if ( !Array.isArray( this.sides ) ) return;
+    if ( this.count !== 4 ) return;
+    let side1 = this.sides[ 0 ]
+    let side2 = this.sides[ 1 ]
+    let side3 = this.sides[ 2 ]
+    let side4 = this.sides[ 3 ]
+    return ( ( side1 === side2 ) && ( side1 === side3 ) && ( side1 === side4 ) )
 
   }
   get area(){
